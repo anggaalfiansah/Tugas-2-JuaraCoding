9ca3a12900ff0271c2090299f5aefc9c01952088
anggaalfiansah <anggaalfiansah26@gmail.com>
2021-01-23 08:59:24 +0700

    Fix Bug Radio button Error

--- src/components/Soal.js
+++ src/components/Soal.js
@@ -2,20 +2,6 @@ import React, { Component } from 'react';
 import { SoalData } from "./SoalData";
 
 class Soal extends Component {
-    // Untuk Validasi Radio
-    constructor(props) {
-        super(props)
-        this.state = {
-            radio: ''
-        }
-    }
-
-    onRadio = (e) => {
-        this.setState({
-            radio: e.currentTarget.value
-        });
-    }
-
     tempatJawaban = (data) => {
         // Jika Tipe Soal Berbentuk essay
         if (data.type === 'essay') {
@@ -23,6 +9,7 @@ class Soal extends Component {
                 <textarea className="form-control"></textarea>
             )
         }
+
         // Jika Tipe Soal Berbentuk checkbox
         else if (data.type === 'checkbox') {
             return (
@@ -38,25 +25,26 @@ class Soal extends Component {
                 </div>
             )
         }
+
         // Jika Tipe Soal Berbentuk radio (a,b,c,d)
         else if (data.type === 'radio') {
             return (
                 <div>
                     <div className="form-check">
-                        <input className="form-check-input" id="a" type="radio" value={data.a} checked={this.state.radio === data.a} onChange={this.onRadio} />
-                        <label className="form-check-label" htmlFor="a">{data.a}</label>
+                        <input className="form-check-input" type="radio" name={data.soal} id={data.a} />
+                        <label className="form-check-label" htmlFor={data.a}>{data.a}</label>
                     </div>
                     <div className="form-check">
-                        <input className="form-check-input" id="b" type="radio" value={data.b} checked={this.state.radio === data.b} onChange={this.onRadio} />
-                        <label className="form-check-label" htmlFor="b">{data.b}</label>
+                        <input className="form-check-input" type="radio" name={data.soal} id={data.b} />
+                        <label className="form-check-label" htmlFor={data.b}>{data.b}</label>
                     </div>
                     <div className="form-check">
-                        <input className="form-check-input" id="c" type="radio" value={data.c} checked={this.state.radio === data.c} onChange={this.onRadio} />
-                        <label className="form-check-label" htmlFor="c">{data.c}</label>
+                        <input className="form-check-input" type="radio" name={data.soal} id={data.c} />
+                        <label className="form-check-label" htmlFor={data.c}>{data.c}</label>
                     </div>
                     <div className="form-check">
-                        <input className="form-check-input" id="d" type="radio" value={data.d} checked={this.state.radio === data.d} onChange={this.onRadio} />
-                        <label className="form-check-label" htmlFor="d">{data.d}</label>
+                        <input className="form-check-input" type="radio" name={data.soal} id={data.d} />
+                        <label className="form-check-label" htmlFor={data.d}>{data.d}</label>
                     </div>
                 </div>
             )
@@ -70,7 +58,9 @@ class Soal extends Component {
                     return (
                         <div className="pt-2" key={key}>
                             <span>{key + 1}. </span><label className="form-label">{data.soal}</label>
-                            {this.tempatJawaban(data)}
+                            <div>
+                                {this.tempatJawaban(data)}
+                            </div>
                             <hr />
                         </div>
                     )
