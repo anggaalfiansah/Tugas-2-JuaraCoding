eedbeefd6eebf39422eb2496cd6fc38b8de9db9a
anggaalfiansah <anggaalfiansah26@gmail.com>
2021-01-25 10:49:34 +0700

    Tambah Soal

--- src/components/SoalData.js
+++ src/components/SoalData.js
@@ -1,51 +1,74 @@
 // Tambah Soal Disini
 export const SoalData = [
-    {
-        type: 'radio',
-        soal: 'Siapa Pemilik Lele Megalodon ?',
-        a: 'Udin Gamboet',
-        b: 'Bozz Sendi',
-        c: 'Otong',
-        d: 'Pakpol',
-        jawab: 'b'
-    },
-    {
-        type: 'radio',
-        soal: 'Siapa Musuh Gamboet BrotherHood ?',
-        a: 'Selili',
-        b: 'Akatsuki',
-        c: 'Sahabat Bulu Burung',
-        d: 'Sunda Empire',
-        jawab: 'a'
-    },
-    {
-        type: 'radio',
-        soal: 'Siapa Yang Suka Minta Gocap Ke Otong ?',
-        a: 'Pein Akatsuki',
-        b: 'Herp',
-        c: 'Derp',
-        d: 'Pakpol',
-        jawab: 'd'
-    },
-    {
-        type: 'checkbox',
-        soal: 'Pilihlah 2 hal yang benar mengenai Maddog Greget',
-        option: ['Greget', 'Musuh Chuck Norris', 'Banci', 'Suka Black Pink'],
-        jawab: ['Greget', 'Musuh Chuck Norris']
-    },
-    {
-        type: 'checkbox',
-        soal: 'Pilihlah 2 hal yang benar mengenai Chuck Norris Banci',
-        option: ['Greget', 'Musuh Maddog Greget', 'Banci', 'Suka Bertarung'],
-        jawab: ['Musuh Maddog Greget', 'Banci']
-    },
-    {
-        type: 'essay',
-        soal: 'Ceritakan Pengalamanmu Ketika Melakukan Hal Greget',
-    },
-    {
-        type: 'essay',
-        soal: 'Apa Pendapatmu Tentang Pakpol Yang Sering Minta Gocap Otong ?',
-    }
+  {
+    type: "radio",
+    soal: "Siapa Pemilik Lele Megalodon ?",
+    a: "Udin Gamboet",
+    b: "Bozz Sendi",
+    c: "Otong",
+    d: "Pakpol",
+    jawab: "b",
+  },
+  {
+    type: "radio",
+    soal: "Siapa Pemilik Jurus Vatok De Snake ?",
+    a: "Udin Gamboet",
+    b: "Bozz Sendi",
+    c: "Otong",
+    d: "Pakpol",
+    jawab: "b",
+  },
+  {
+    type: "radio",
+    soal: "Siapa Musuh Gamboet BrotherHood ?",
+    a: "Selili",
+    b: "Akatsuki",
+    c: "Sahabat Bulu Burung",
+    d: "Sunda Empire",
+    jawab: "a",
+  },
+  {
+    type: "radio",
+    soal: "Siapa Yang Suka Minta Gocap Ke Otong ?",
+    a: "Pein Akatsuki",
+    b: "Herp",
+    c: "Derp",
+    d: "Pakpol",
+    jawab: "d",
+  },
+  {
+    type: "checkbox",
+    soal: "Pilihlah 2 hal yang benar mengenai Maddog Greget",
+    option: ["Greget", "Musuh Chuck Norris", "Banci", "Suka Black Pink"],
+    jawab: ["Greget", "Musuh Chuck Norris"],
+  },
+  {
+    type: "checkbox",
+    soal: "Pilihlah 2 hal yang benar mengenai Chuck Norris Banci",
+    option: ["Greget", "Musuh Maddog Greget", "Banci", "Suka Bertarung"],
+    jawab: ["Musuh Maddog Greget", "Banci"],
+  },
+  {
+    type: "checkbox",
+    soal: "Pilihlah 2 hal yang benar mengenai Pakpol",
+    option: [
+      "Pakpol suka minta gocap ke Otong",
+      "Pakpol Jujur",
+      "Pakpol Pungli",
+      "Pakpol Hebat",
+    ],
+    jawab: ["Pakpol suka minta gocap ke Otong", "Pakpol Pungli"],
+  },
+  {
+    type: "essay",
+    soal: "Ceritakan Pengalamanmu Ketika Melakukan Hal Greget",
+  },
+  {
+    type: "essay",
+    soal: "Apa Pendapatmu Tentang Pakpol Yang Sering Minta Gocap Otong ?",
+  },
+  {
+    type: "essay",
+    soal: "Apa Pendapatmu Tentang Udin Gamboet ?",
+  },
 ];
-
