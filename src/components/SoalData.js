edd9f8539f673e7fb0500044c24eea2d8e588f01
anggaalfiansah <anggaalfiansah26@gmail.com>
2021-01-25 10:33:56 +0700

    tambah soal

--- src/components/SoalData.js
+++ src/components/SoalData.js
@@ -1,51 +1,60 @@
 // Tambah Soal Disini
 export const SoalData = [
     {
-        type: 'radio',
-        soal: 'Siapa Pemilik Lele Megalodon ?',
-        a: 'Udin Gamboet',
-        b: 'Bozz Sendi',
-        c: 'Otong',
-        d: 'Pakpol',
-        jawab: 'b'
+        "type": 'radio',
+        "soal": 'Siapa Pemilik Lele Megalodon ?',
+        "a": 'Udin Gamboet',
+        "b": 'Bozz Sendi',
+        "c": 'Otong',
+        "d": 'Pakpol',
+        "jawab": 'b'
     },
     {
-        type: 'radio',
-        soal: 'Siapa Musuh Gamboet BrotherHood ?',
-        a: 'Selili',
-        b: 'Akatsuki',
-        c: 'Sahabat Bulu Burung',
-        d: 'Sunda Empire',
-        jawab: 'a'
+        "type": 'radio',
+        "soal": 'Siapa Pemilik Jurus Vatok De Snake ?',
+        "a": 'Udin Gamboet',
+        "b": 'Bozz Sendi',
+        "c": 'Otong',
+        "d": 'Pakpol',
+        "jawab": 'b'
     },
     {
-        type: 'radio',
-        soal: 'Siapa Yang Suka Minta Gocap Ke Otong ?',
-        a: 'Pein Akatsuki',
-        b: 'Herp',
-        c: 'Derp',
-        d: 'Pakpol',
-        jawab: 'd'
+        "type": 'radio',
+        "soal": 'Siapa Musuh Gamboet BrotherHood ?',
+        "a": 'Selili',
+        "b": 'Akatsuki',
+        "c": 'Sahabat Bulu Burung',
+        "d": 'Sunda Empire',
+        "jawab": 'a'
     },
     {
-        type: 'checkbox',
-        soal: 'Pilihlah 2 hal yang benar mengenai Maddog Greget',
-        option: ['Greget', 'Musuh Chuck Norris', 'Banci', 'Suka Black Pink'],
-        jawab: ['Greget', 'Musuh Chuck Norris']
+        "type": 'radio',
+        "soal": 'Siapa Yang Suka Minta Gocap Ke Otong ?',
+        "a": 'Pein Akatsuki',
+        "b": 'Herp',
+        "c": 'Derp',
+        "d": 'Pakpol',
+        "jawab": 'd'
     },
     {
-        type: 'checkbox',
-        soal: 'Pilihlah 2 hal yang benar mengenai Chuck Norris Banci',
-        option: ['Greget', 'Musuh Maddog Greget', 'Banci', 'Suka Bertarung'],
-        jawab: ['Musuh Maddog Greget', 'Banci']
+        "type": 'checkbox',
+        "soal": 'Pilihlah 2 hal yang benar mengenai Maddog Greget',
+        "option": ['Greget', 'Musuh Chuck Norris', 'Banci', 'Suka Black Pink'],
+        "jawab": ['Greget', 'Musuh Chuck Norris']
     },
     {
-        type: 'essay',
-        soal: 'Ceritakan Pengalamanmu Ketika Melakukan Hal Greget',
+        "type": 'checkbox',
+        "soal": 'Pilihlah 2 hal yang benar mengenai Chuck Norris Banci',
+        "option": ['Greget', 'Musuh Maddog Greget', 'Banci', 'Suka Bertarung'],
+        "jawab": ['Musuh Maddog Greget', 'Banci']
     },
     {
-        type: 'essay',
-        soal: 'Apa Pendapatmu Tentang Pakpol Yang Sering Minta Gocap Otong ?',
+        "type": 'essay',
+        "soal": 'Ceritakan Pengalamanmu Ketika Melakukan Hal Greget',
+    },
+    {
+        "type": 'essay',
+        "soal": 'Apa Pendapatmu Tentang Pakpol Yang Sering Minta Gocap Otong ?',
     }
 ];
 
